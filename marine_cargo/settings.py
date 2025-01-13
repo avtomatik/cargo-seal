5654778da135c82b40a3dc3a19ce37a10d124ba1
Alexander Mikhailov <extroper@yandex.ru>
2025-01-13 09:40:29 +0300

    Added Environment Variables

--- marine_cargo/settings.py
+++ marine_cargo/settings.py
@@ -10,17 +10,28 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# =============================================================================
+# Loads Environment Variables
+# =============================================================================
+load_dotenv()
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p@x(30lfu04_v$yt(rnnt^bllxt9y%gc^n&+mku*t5pvg(sqz$'
+SECRET_KEY = os.getenv(
+    'SECRET_KEY',
+    'django-insecure-!#$%&&&(()**+000000000=@^aaaaaaaaaaaaaaaaaaaaaaaaa'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
