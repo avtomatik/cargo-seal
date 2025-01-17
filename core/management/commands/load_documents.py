992b4f120e0c8c4b5a2f54704af04930684a73a9
aarchitektt <axel.schrijver@gmail.com>
2025-01-17 22:52:10 +0300

    Added `load_documents` Command

---
+++ core/management/commands/load_documents.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 17 22:15:36 2025
+
+@author: Aleksandr.Mikhailov
+"""
+
+import csv
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from vessels.models import Document
+
+
+class Command(BaseCommand):
+
+    PATH_DATA = 'data'
+
+    FILE_NAME = 'vessels_document.csv'
+
+    FIELD_NAMES = ['number', 'category', 'date', 'provider_id', 'vessel_id']
+
+    PATH = settings.BASE_DIR.joinpath(PATH_DATA)
+
+    help = 'Populates DB with Vessel Documents Data'
+
+    def handle(self, *args, **options):
+
+        with open(self.PATH.joinpath(self.FILE_NAME), encoding='utf8') as file:
+            reader = csv.DictReader(file, fieldnames=self.FIELD_NAMES)
+            Document.objects.bulk_create(
+                (Document(**_) for _ in reader),
+                ignore_conflicts=True
+            )
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                'Successfully Populated DB with Vessel Documents Data')
+        )
