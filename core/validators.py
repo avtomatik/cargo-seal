43080b983f9bff68274167e2a9090ebffd0042f8
Alexander Mikhailov <extroper@yandex.ru>
2025-01-19 23:06:00 +0300

    Impemented `IMOValidator`

--- core/validators.py
+++ core/validators.py
@@ -6,32 +6,22 @@ Created on Sun Jan 12 16:45:18 2025
 @author: alexandermikhailov
 """
 
+from operator import mul
+
 from django.core.exceptions import ValidationError
 
 
 class IMOValidator:
-    """TODO: Implement."""
 
     def __call__(self, value):
-        if not value:
+
+        numbers = []
+
+        for _ in range(7):
+            value, number = divmod(value, 10)
+            numbers.append(number)
+
+        number = numbers.pop(0)
+
+        if sum(map(mul, numbers, range(2, 8))) % 10 != number:
             raise ValidationError('Not A Valid IMO-Number.')
-
-
-def is_valid_imo(imo: str) -> bool:
-    """
-    Checks If Valid IMO Number
-
-    Parameters
-    ----------
-    imo : str
-        DESCRIPTION.
-
-    Returns
-    -------
-    bool
-        DESCRIPTION.
-
-    """
-    return sum(
-        map(lambda _: _[0] * int(_[1]), enumerate(imo[::-1][1:], start=2))
-    ) % 10 == int(imo[-1])
