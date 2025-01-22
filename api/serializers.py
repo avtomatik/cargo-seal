62f87fd5b449e1190e99e1d40af16fd8d29b4886
aarchitektt <axel.schrijver@gmail.com>
2025-01-22 16:49:30 +0300

    Added `FormFieldsSerializer` Stub

--- api/serializers.py
+++ api/serializers.py
@@ -12,6 +12,31 @@ class CoverageSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class FormFieldsSerializer(serializers.Serializer):
+
+    deal_number = serializers.CharField(max_length=16)
+    insured = serializers.CharField(max_length=16)
+    address = serializers.CharField(max_length=16)
+    beneficiary = serializers.CharField(max_length=16)
+    beneficiary_address = serializers.CharField(max_length=16)
+    surveyor_disport = serializers.CharField(max_length=16)
+    surveyor_loadport = serializers.CharField(max_length=16)
+    basis_of_valuation = serializers.CharField(max_length=16)
+    date = serializers.CharField(max_length=16)
+    bl_number = serializers.CharField(max_length=16)
+    bl_date = serializers.CharField(max_length=16)
+    disport = serializers.CharField(max_length=16)
+    loadport = serializers.CharField(max_length=16)
+    policy_number = serializers.CharField(max_length=16)
+    policy_date = serializers.CharField(max_length=16)
+    subject_matter_insured = serializers.CharField(max_length=16)
+    vessel = serializers.CharField(max_length=16)
+    imo = serializers.CharField(max_length=16)
+    year_built = serializers.CharField(max_length=16)
+    sum_insured = serializers.CharField(max_length=16)
+    weight_metric = serializers.CharField(max_length=16)
+
+
 class PartySerializer(serializers.ModelSerializer):
 
     class Meta:
