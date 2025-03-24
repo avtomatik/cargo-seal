6e34aecd37b0d774f7b3d2790c8969d44d4c1162
aarchitektt <axel.schrijver@gmail.com>
2025-03-24 19:07:31 +0300

    Updated `FormMergeSerializer`

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from coverage.models import Coverage, Policy
+from logistics.models import Shipment
 from procurement.models import Party
 from vessels.models import Document, Vessel
 
@@ -42,34 +43,66 @@ class CoverageSerializer(serializers.ModelSerializer):
 
 class FormMergeSerializer(serializers.Serializer):
 
-    deal_number = serializers.CharField(max_length=16)
-    insured = serializers.CharField(max_length=16)
-    address = serializers.CharField(max_length=16)
-    beneficiary = serializers.CharField(max_length=16)
-    beneficiary_address = serializers.CharField(max_length=16)
-    surveyor_disport = serializers.CharField(max_length=16)
-    surveyor_loadport = serializers.CharField(max_length=16)
-    basis_of_valuation = serializers.CharField(max_length=16)
-    date = serializers.CharField(max_length=16)
-    bl_number = serializers.CharField(max_length=16)
-    bl_date = serializers.CharField(max_length=16)
-    disport = serializers.CharField(max_length=16)
+    deal_number = serializers.CharField(source='number')
+    # insured = serializers.CharField(max_length=16)
+    # address = serializers.CharField(max_length=16)
+    # beneficiary = serializers.CharField(max_length=16)
+    # beneficiary_address = serializers.CharField(max_length=16)
+    surveyor_loadport = serializers.CharField(source='surveyor_loadport.name')
+    surveyor_disport = serializers.CharField(source='surveyor_disport.name')
+    # basis_of_valuation = serializers.CharField(max_length=16)
+    # date = serializers.CharField(max_length=16)
+    # bl_number = serializers.CharField(source='number')
+    bl_date = serializers.CharField(source='date')
     loadport = serializers.CharField(max_length=16)
-    policy_number = serializers.CharField(max_length=16)
-    policy_date = serializers.CharField(max_length=16)
+    disport = serializers.CharField(max_length=16)
+    policy_number = serializers.CharField(source='coverage.policy.number')
+    policy_date = serializers.CharField(source='coverage.policy.inception')
     subject_matter_insured = serializers.CharField(max_length=16)
-    vessel = serializers.CharField(max_length=16)
-    imo = serializers.CharField(max_length=16)
-    year_built = serializers.CharField(max_length=16)
-    sum_insured = serializers.CharField(max_length=16)
-    weight_metric = serializers.CharField(max_length=16)
+    vessel = serializers.CharField(source='vessel.name')
+    imo = serializers.CharField(source='vessel.imo')
+    year_built = serializers.CharField(source='vessel.built_on')
+    weight_metric = serializers.SerializerMethodField(read_only=True)
+    sum_insured = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Shipment
+        fields = [
+            'deal_number',
+            'insured',
+            'address',
+            'beneficiary',
+            'beneficiary_address',
+            'surveyor_loadport',
+            'surveyor_disport',
+            'basis_of_valuation',
+            'date',
+            'bl_number',
+            'bl_date',
+            'loadport',
+            'disport',
+            'policy_number',
+            'policy_date',
+            'subject_matter_insured',
+            'vessel',
+            'imo',
+            'year_built',
+            'weight_metric',
+            'sum_insured',
+        ]
+
+    def get_weight_metric(self, obj):
+        return f'{obj.unit} {float(obj.weight_metric):,.3f}'
+
+    def get_sum_insured(self, obj):
+        return f'{obj.ccy} {float(obj.sum_insured):,.2f}'
 
 
 class VesselSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Vessel
-        fields = '__all__'
+        fields = ['name', 'imo', 'built_on']
 
 
 class DocumentSerializer(serializers.ModelSerializer):
