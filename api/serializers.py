e73e1b98fcd41c604aa703361acc7ed3109475b9
aarchitektt <axel.schrijver@gmail.com>
2025-03-26 18:15:07 +0300

    Updated `FormMergeSerializer`

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,18 @@
+from django.utils import timezone
 from rest_framework import serializers
 
 from coverage.models import Coverage, Policy
 from logistics.models import Shipment
 from procurement.models import Party
 from vessels.models import Document, Vessel
+
+
+class BillOfLadingSerializer(serializers.Serializer):
+
+    bl_number = serializers.CharField(source='number')
+
+    class Meta:
+        model = Shipment
 
 
 class PartySerializer(serializers.ModelSerializer):
@@ -44,15 +53,25 @@ class CoverageSerializer(serializers.ModelSerializer):
 class FormMergeSerializer(serializers.Serializer):
 
     deal_number = serializers.CharField(source='number')
-    # insured = serializers.CharField(max_length=16)
-    # address = serializers.CharField(max_length=16)
-    # beneficiary = serializers.CharField(max_length=16)
-    # beneficiary_address = serializers.CharField(max_length=16)
+    insured = serializers.CharField(source='coverage.policy.insured')
+    address = serializers.CharField(source='coverage.policy.insured.address')
+    beneficiary = serializers.CharField(source='contract.buyer')
+    beneficiary_address = serializers.CharField(
+        source='contract.buyer.address'
+    )
     surveyor_loadport = serializers.CharField(source='surveyor_loadport.name')
     surveyor_disport = serializers.CharField(source='surveyor_disport.name')
-    # basis_of_valuation = serializers.CharField(max_length=16)
-    # date = serializers.CharField(max_length=16)
-    # bl_number = serializers.CharField(source='number')
+    basis_of_valuation = serializers.SerializerMethodField()
+    date = serializers.SerializerMethodField()
+    # bl_number = BillOfLadingSerializer(many=True)
+# =============================================================================
+#     TODO: Implement Handle to Generate the Following Output:
+#         "bl_number": [
+#             {
+#                 "bl_number": "Bill of Lading #\u00a0SILNVS25117500 dated 19\u00a0March\u00a02025"
+#             }
+#         ],
+# =============================================================================
     bl_date = serializers.CharField(source='date')
     loadport = serializers.CharField(max_length=16)
     disport = serializers.CharField(max_length=16)
@@ -61,7 +80,7 @@ class FormMergeSerializer(serializers.Serializer):
     subject_matter_insured = serializers.CharField(max_length=16)
     vessel = serializers.CharField(source='vessel.name')
     imo = serializers.CharField(source='vessel.imo')
-    year_built = serializers.CharField(source='vessel.built_on')
+    year_built = serializers.SerializerMethodField()
     weight_metric = serializers.SerializerMethodField(read_only=True)
     sum_insured = serializers.SerializerMethodField(read_only=True)
 
@@ -91,11 +110,21 @@ class FormMergeSerializer(serializers.Serializer):
             'sum_insured',
         ]
 
-    def get_weight_metric(self, obj):
-        return f'{obj.unit} {float(obj.weight_metric):,.3f}'
+    def get_basis_of_valuation(self, obj):
+        return '100%'
+
+    def get_date(self, obj):
+        today = timezone.now().date()
+        return min(obj.date, today)
 
     def get_sum_insured(self, obj):
         return f'{obj.ccy} {float(obj.sum_insured):,.2f}'
+
+    def get_weight_metric(self, obj):
+        return f'{obj.weight_metric:,.3f}'
+
+    def get_year_built(self, obj):
+        return f'{obj.vessel.built_on.year}'
 
 
 class VesselSerializer(serializers.ModelSerializer):
